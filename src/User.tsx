c66c22e083b0b40c102f917b45c3d95cbc5ff0d1
jacobhsu <jacob.hsu.tw@gmail.com>
2022-08-08 17:32:34 +0800

    feat: facebook profile avatar

--- src/User.tsx
+++ src/User.tsx
@@ -12,6 +12,7 @@ export const User = memo(({ provider, profile, onLogout }: Props) => {
     profile?.profile_image_url ||
     profile?.avatar_url ||
     profile?.picture ||
+    profile?.picture?.url ||
     profile?.picture?.data?.url ||
     profile?.profile_image_url_https ||
     'https://maxcdn.icons8.com/Share/icon/p1em/users//gender_neutral_user1600.png'
